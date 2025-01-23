0b325edfa36d3fa0340e03b932c87f43921096cc
Jorge Davi Navarro <jorgetbg@hotmail.com>
2025-01-23 18:03:05 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
-function Home(){
-    return <h1>Amor da minha vida, eu te amo muito minha delicinha. Te AMO. ass. Jd</h1>
+function Home() {
+  return (
+    <h1>
+      Amor da minha vida, eu te amo muito minha delicinha. Te AMO. ass. Jd
+    </h1>
+  );
 }
-
 
 export default Home;
